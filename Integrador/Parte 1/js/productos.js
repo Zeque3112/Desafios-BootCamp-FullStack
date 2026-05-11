9506dcd4136e7fd7a51b2611262943410ca43124
Zeque3112 <crecyer@yahoo.com>
2026-05-11 12:09:34 -0300

    feat: acomodo carpetas para mejor organizacion

---
+++ Integrador/Parte 1/js/productos.js
@@ -0,0 +1,53 @@
+const productos = [
+  {
+    nombre: "Notebook Lenovo IdeaPad 3",
+    precio: 850000,
+    stock: 12,
+    marca: "Lenovo",
+    categoria: "Notebook",
+    detalles: "Pantalla 15.6'', Intel Core i5, 8GB RAM, SSD 512GB",
+    envio: true,
+    foto: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8"
+  },
+  {
+    nombre: "Mouse Logitech G203",
+    precio: 32000,
+    stock: 45,
+    marca: "Logitech",
+    categoria: "Perifericos",
+    detalles: "Mouse gamer RGB, 8000 DPI, conexión USB",
+    envio: true,
+    foto: "https://www.xt-pc.com.ar/img/productos/14/MOU454.jpg"
+  },
+  {
+    nombre: "Teclado Redragon Kumara K552",
+    precio: 78000,
+    stock: 20,
+    marca: "Redragon",
+    categoria: "Perifericos",
+    detalles: "Teclado mecánico, switches Blue, retroiluminado",
+    envio: true,
+    foto: "https://images.unsplash.com/photo-1519389950473-47ba0277781c"
+  },
+  {
+    nombre: "Monitor Samsung 24 FHD",
+    precio: 210000,
+    stock: 8,
+    marca: "Samsung",
+    categoria: "Monitores",
+    detalles: "Resolución 1920x1080, panel IPS, 75Hz",
+    envio: false,
+    foto: "https://images.unsplash.com/photo-1527443224154-c4a3942d3acf"
+  },
+  {
+    nombre: "Disco SSD Kingston 1TB",
+    precio: 95000,
+    stock: 30,
+    marca: "Kingston",
+    categoria: "Almacenamiento",
+    detalles: "SSD SATA 2.5'', velocidad hasta 550MB/s",
+    envio: true,
+    foto: "https://images.unsplash.com/photo-1597872200969-2b65d56bd16b"
+  }
+];
+
